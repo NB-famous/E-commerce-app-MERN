0c39052f600ac7cf5422f8f62294d04f84cf6a77
NB-famous <ncbadoles@gmail.com>
2020-11-08 14:01:51 -0500

    use html template and created a react app and making it dynamic

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Nice suit',
+            category:'suits',
+            image:'/images/product-1.jpg',
+            price: 300,
+            brand: 'Versachi',
+            rating: 4.3,
+            numReviews: 10,
+            description: 'Good looking suit',
+        },
+
+        {
+            _id:'2',
+            name: 'Shirt',
+            category:'shirt',
+            image:'/images/product-2.jpg',
+            price: 100,
+            brand: 'Versachi',
+            rating: 4.7,
+            numReviews: 10,
+            description: 'Good looking suit',
+        },
+
+        {
+            _id:'3',
+            name: 'Jacket',
+            category:'suit',
+            image:'/images/product-3.jpg',
+            price: 200,
+            brand: 'Versachi',
+            rating: 3.3,
+            numReviews: 10,
+            description: 'Good looking suit',
+        },
+
+        {
+            _id:'4',
+            name: 'Pants',
+            category:'pants',
+            image:'/images/product-1.jpg',
+            price: 150,
+            brand: 'Versachi',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Good looking suit',
+        },
+    ]
+}
+
+export default data
