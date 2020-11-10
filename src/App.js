c307f315008336a617c9e538b08892d3557b2784
NB-famous <ncbadoles@gmail.com>
2020-11-09 20:50:59 -0500

    part 7 building rating and product component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import data from './data';
+import Product from './components/Product'
 
 function App() {
   return (
     <div className="grid-container">
     <header className="row">
         <div>
-            <a className="brand" href="index.html">amazona</a>
+            <a className="brand" href="/">amazona</a>
         </div>
         <div>
-            <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a> 
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a> 
         </div>
     </header>
     <main>
@@ -18,30 +19,7 @@ function App() {
 
           {
             data.products.map(product => (
-
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                   
-                    <img className="medium" src={product.image} alt={product.name}/>
-                </a>
-                
-                <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2>
-                    </a>
-                </div>
-
-                <div className="rating">
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>        
-                </div>
-
-                <div className="price"> {product.price} $ </div>
-            </div> 
-
+               <Product key={product._id} product={product} />
             ))
           } 
         </div>
