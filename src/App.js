44fd11cff40a26bfd86ab3b55d008e0e901d40f1
NB-famous <ncbadoles@gmail.com>
2020-11-14 12:26:03 -0500

    Part 8 complete created homescreen and product screen components and added react router dom

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+// Imported Components ///
+import ProductScreen from './screens/ProductScreen'
+import HomeScreen from './screens/HomeScreen'
+
 
 function App() {
   return (
-    <div className="grid-container">
-    <header className="row">
-        <div>
-            <a className="brand" href="/">amazona</a>
-        </div>
-        <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a> 
-        </div>
-    </header>
-    <main>
-        <div className="row center">
-
-          {
-            data.products.map(product => (
-               <Product key={product._id} product={product} />
-            ))
-          } 
-        </div>
-    </main>
-    <footer className="row center">All rights & reserved</footer>
-    
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+      <header className="row">
+          <div>
+              <a className="brand" href="/">amazona</a>
+          </div>
+          <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a> 
+          </div>
+      </header>
+      <main>
+          <Route path="/product/:id" component={ProductScreen} exact></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+      </main>
+      <footer className="row center">All rights & reserved</footer>
+      
+      </div>
+    </BrowserRouter>
   );
 }
 
