b5108287320b62897855447e9e6a784fcfcf44ea
NB-famous <ncbadoles@gmail.com>
2020-12-20 11:32:33 -0500

    completed task 38 building the edit product screen

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -18,6 +18,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import PrivateRoute from './components/PrivateRoute';
 import AdminRoute from './components/AdminRoute';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 
 function App() {
@@ -95,6 +96,7 @@ function App() {
       <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen} exact></Route>
+          <Route path="/product/:id/edit" component={ProductEditScreen} exact></Route>
           <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
