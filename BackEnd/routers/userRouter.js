13c4d16cdc824e17c633a2c6d2043204e0798cc4
NB-famous <ncbadoles@gmail.com>
2021-01-04 15:18:56 -0500

    completed task 47 implimented admin user list delete feature added constraint on admin users

--- BackEnd/routers/userRouter.js
+++ BackEnd/routers/userRouter.js
@@ -95,7 +95,8 @@ userRouter.delete('/:id', isAuth, isAdmin, expressAsyncHandler(async(req,res) =>
     const user = await User.findById(req.params.id);
     if(user) {
         if(user.email === 'adming@example.com'){
-            res.send({message:'Can Not Delete Admin'})
+            res.status(400).send({message:'Can Not Delete Admin User'})
+            return;
         }
         const deleteUser = await user.remove();
         res.send({message: 'User Deleted', user: deleteUser});
