7a4c1edafbbfa52370688f9c626bfa4c559dc3a1
NB-famous <ncbadoles@gmail.com>
2021-01-03 20:23:49 -0500

    completing task 46 applying admin user list feature

--- BackEnd/routers/userRouter.js
+++ BackEnd/routers/userRouter.js
@@ -10,7 +10,7 @@ const userRouter = express.Router();
 userRouter.get('/seed', expressAsyncHandler(async(req, res) => {
 
     // this will delete and reseed your database each refresh -> comment this out once data base is reseeded
-    //await User.remove({});
+    await User.remove({});
     /////////////////
     const createdUsers = await User.insertMany(data.users);
     res.send({
